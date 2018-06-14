cbee905d3de3eb22a5c9939acba076bde7dee6f9
JLChnToZ <jeremylam619@gmail.com>
2018-06-14 23:50:52 +0800

    Bump to 0.4 Fix incorrect translation

--- ClipboardManager/Properties/AssemblyInfo.cs
+++ ClipboardManager/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("2026725f-bb44-4bdc-a85b-d2326f7ec716")]
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
